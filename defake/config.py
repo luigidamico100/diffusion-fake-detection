bb2d35364fdd786374ca3453e92d5e07f6c2e672
luigidamico100 <luigidamico.1010@gmail.com>
2023-01-17 21:56:58 +0100

    Seed function

--- defake/config.py
+++ defake/config.py
@@ -1,4 +1,7 @@
 import torch
+import random, os
+import numpy as np
+
 
 if torch.cuda.is_available():
 	device = torch.device("cuda")
@@ -8,3 +11,19 @@ elif torch.backends.mps.is_available():
 	# device = torch.device("cpu")
 else:
 	torch.device("cpu")
+    
+    
+def seed_everything(seed: int):
+    '''
+    Disabling the benchmarking feature with torch.backends.cudnn.benchmark = False causes cuDNN to 
+    deterministically select an algorithm, possibly at the cost of reduced performance.
+    However, if you do not need reproducibility across multiple executions of your application, 
+    then performance might improve if the benchmarking feature is enabled with torch.backends.cudnn.benchmark = True.
+    '''    
+    random.seed(seed)
+    os.environ['PYTHONHASHSEED'] = str(seed)
+    np.random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = True
